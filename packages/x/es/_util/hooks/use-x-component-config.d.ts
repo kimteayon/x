4a8ff9b1b17c029f217ca880e49461cf2aa68563
Mickey <951203214@qq.com>
2025-06-07 17:16:13 +0800

    feat: shortkey of conversations

--- packages/x/es/_util/hooks/use-x-component-config.d.ts
+++
@@ -1,3 +0,0 @@
-import type { XComponentConfig, XComponentsConfig } from '../../x-provider/context';
-declare const useXComponentConfig: <C extends keyof XComponentsConfig>(component: C) => Required<XComponentsConfig>[C] & XComponentConfig;
-export default useXComponentConfig;
